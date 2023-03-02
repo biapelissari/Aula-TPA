4625e2191e87aaeacf42c8eaf6261e3897104faf
biapelissari <126783952+biapelissari@users.noreply.github.com>
2023-03-02 14:14:34 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php  echo"<h1>Ola mundo</h1>"?>
+    <h2>8+8 = <?php   echo(8 + 8)?></h2> 
+    <h2>8-4 = <?php   echo(8 - 4)?></h2> 
+    <h2>8*8 = <?php   echo(8 * 8)?></h2> 
+    <h2>8/4 = <?php   echo(8 / 4)?></h2> 
+</body>
+</html>
